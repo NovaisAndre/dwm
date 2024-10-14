b227aaaff96da5b4d3304a22e9e5f80f9c22178a
AndreNovaisBrito <novaisandre.dev@gmail.com>
2024-10-14 18:00:01 -0300

    Auto commit on 2024-10-14 18:00:01

--- config.h
+++ config.h
@@ -112,8 +112,8 @@ static const Layout layouts[] = {
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 #define STACKKEYS(MOD,ACTION) \
-	{ MOD,	XK_j,	ACTION##stack,	{.i = INC(+1) } }, \
-	{ MOD,	XK_k,	ACTION##stack,	{.i = INC(-1) } }, \
+	{ MOD,	XK_n,	ACTION##stack,	{.i = INC(+1) } }, \
+	{ MOD,	XK_p,	ACTION##stack,	{.i = INC(-1) } }, \
 	{ MOD,  XK_v,   ACTION##stack,  {.i = 0 } }, \
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
