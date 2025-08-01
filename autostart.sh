a1533dd76585c96b43e60650acde6c9b2f30b5ff
Andre Novais <novaisandre.dev@gmail.com>
2025-08-01 12:00:01 -0300

    Auto commit on 2025-08-01 12:00:01

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-picom &
+picom --config ~/dotfiles/.config/picom/picom.conf &
 ~/dotfiles/.scripts/cycle_wallpapers.sh &
 dunst &
 xrandr --output eDP-1 --primary --mode 1366x768 --pos 277x1080 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal &
