b5bf935835bef1997103d14b614dee2e8b027f52
André Novais <novaisandre.dev@gmail.com>
2025-09-14 23:00:01 -0300

    Auto commit on 2025-09-14 23:00:01

--- autostart.sh
+++ autostart.sh
@@ -5,12 +5,10 @@ tmux has-session -t fastterm 2>/dev/null || tmux new-session -c ~ -d -s fastterm
 picom --config ~/dotfiles/.config/picom/picom.conf &
 ~/dotfiles/.scripts/cycle_wallpapers.sh &
 dunst &
-xrandr --output eDP-1 --primary --mode 1366x768 --pos 277x1080 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal &
+#xrandr --output eDP-1 --primary --mode 1366x768 --pos 277x1080 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal &
 xmodmap ~/.Xmodmap &
-redshift -l -3:-38 & 
-#xinput --set-prop 13 'Coordinate Transformation Matrix' 0.5 0 0 0 0.5 0 0 0 1 &
-#xinput set-prop 11 "libinput Tapping Enabled" 1
-#xinput set-prop 15 "libinput Tapping Enabled" 1
+#redshift -l -3:-38 & 
+redshift -m vidmode -l -3.7319:-38.5267
 #xbindkeys &
 #dwmblocks & 
 # ibus-daemon -rxRd &
